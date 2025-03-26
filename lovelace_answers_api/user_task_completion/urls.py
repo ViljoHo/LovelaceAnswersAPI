e20dc9920180e057e6c676adc4e5c86b2745dbc8
Kasperi <kasperi.ervasti@gmail.com>
2025-03-26 14:08:27 +0200

    All completions HTTP methods implemented

--- lovelace_answers_api/user_task_completion/urls.py
+++ lovelace_answers_api/user_task_completion/urls.py
@@ -1,36 +1,37 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path(
         "completions/",
         views.UserTaskCompletionListCreate.as_view(),
-        name="completions-view-create",
+        name="get-post-completions",
     ),
     path(
         "users/<str:user>/completions/",
         views.UserTaskCompletionListByUser.as_view(),
-        name="completions-by-user",
+        name="get-completions-by-user",
     ),
     path(
         "exercises/<str:exercise>/completions/",
         views.UserTaskCompletionListByExercise.as_view(),
-        name="completions-by-exercise",
+        name="get-completions-by-exercise",
     ),
     path(
         "courses/<str:course>/completions/",
         views.UserTaskCompletionListByCourse.as_view(),
-        name="completions-by-course",
+        name="get-completions-by-course",
     ),
     # path("users/<str:user>/exercises/<str:exercise>/completions/", views.UserTaskCompletionListByUserExercise.as_view(), name="completions-by-user-exercise"),
     path(
         "users/<str:user>/courses/<str:course>/completions/",
         views.UserTaskCompletionListByUserCourse.as_view(),
-        name="completions-by-user-course",
+        name="get-completions-by-user-course",
     ),
     path(
         "users/<str:user>/exercises/<str:exercise>/courses/<str:instance>/completions/",
         views.UserTaskCompletionRetrieveUpdateDestroy.as_view(),
-        name="completions-by-user-exercise-course",
+        name="get-put-patch-delete-completion",
     ),
 ]
--- lovelace_answers_api/user_task_completion/views.py
+++ lovelace_answers_api/user_task_completion/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import get_object_or_404, render
 from rest_framework import generics
 from .models import UserTaskCompletion
 from .serializers import UserTaskCompletionSerializer
+
+from .utils import MultipleFieldLookupMixin
 
 
 class UserTaskCompletionListCreate(generics.ListCreateAPIView):
@@ -44,24 +45,6 @@ class UserTaskCompletionListByUserCourse(generics.ListAPIView):
         user = self.kwargs['user']
         course = self.kwargs['course']
         return UserTaskCompletion.objects.filter(user=user, instance=course)
-    
-class MultipleFieldLookupMixin:
-    """
-    FROM: https://www.django-rest-framework.org/api-guide/generic-views/#creating-custom-mixins
-
-    Apply this mixin to any view or viewset to get multiple field filtering
-    based on a `lookup_fields` attribute, instead of the default single field filtering.
-    """
-    def get_object(self):
-        queryset = self.get_queryset()             # Get the base queryset
-        queryset = self.filter_queryset(queryset)  # Apply any filter backends
-        filter = {}
-        for field in self.lookup_fields:
-            if self.kwargs.get(field): # Ignore empty fields.
-                filter[field] = self.kwargs[field]
-        obj = get_object_or_404(queryset, **filter)  # Lookup the object
-        self.check_object_permissions(self.request, obj)
-        return obj
 
 class UserTaskCompletionRetrieveUpdateDestroy(MultipleFieldLookupMixin, generics.RetrieveUpdateDestroyAPIView):
     queryset = UserTaskCompletion.objects.all()
