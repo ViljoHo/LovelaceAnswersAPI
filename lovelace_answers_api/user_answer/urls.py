4aa4d222d81068606991c738d969f8a481a20515
Viljo Holma <viljo.holma@gmail.com>
2025-03-26 12:05:51 +0200

    user_answer get and post methods works

--- lovelace_answers_api/user_answer/urls.py
+++ lovelace_answers_api/user_answer/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("answers/", views.UserAnswerListView.as_view(), name="useranswer-view-list"),
-    path("answers/textfield/", views.UserTextfieldExerciseAnswerCreateView.as_view(), name="textfieldanswer-view-create"),
-    path("users/<str:user>/answers/", views.SpesificUserAnswerListView.as_view(), name="get-by-user"),
-    path("exercises/<str:exercise>/answers/", views.SpesificExerciseAnswerListView.as_view(), name="get-by-exercise")
+    path("answers/", views.AnswerList.as_view(), name="useranswer-view-list"),
+    path("answers/textfield/", views.UserTextfieldExerciseAnswerCreate.as_view(), name="textfieldanswer-view-create"),
+    path("users/<str:user>/answers/", views.AnswerListByUser.as_view(), name="get-by-user"),
+    path("exercises/<str:exercise>/answers/", views.AnswerListByExercise.as_view(), name="get-by-exercise"),
+    path("courses/<str:course>/answers/", views.AnswerListByCourse.as_view(), name="get-by-course"),
+    path("users/<str:user>/exercises/<str:exercise>/answers/", views.AnswerListByUserAndExercise.as_view(), name="get-by-user-and-exercise"),
+    path("users/<str:user>/courses/<str:course>/answers/", views.AnswerListByUserAndCourse.as_view(), name="get-by-user-and-course"),
+    path("answers/<int:id>/", views.SpecificAnswer.as_view(), name="get-specific-answer")
 ]
--- lovelace_answers_api/user_answer/views.py
+++ lovelace_answers_api/user_answer/views.py
@@ -6,38 +6,72 @@ from rest_framework.views import APIView
 
 
 
-class UserAnswerListView(generics.ListAPIView):
+class AnswerList(generics.ListAPIView):
   queryset = UserAnswer.objects.all()
   serializer_class = DynamicUserAnswerSerializer
 
-class UserTextfieldExerciseAnswerCreateView(generics.CreateAPIView):
+class UserTextfieldExerciseAnswerCreate(generics.CreateAPIView):
     queryset = UserTextfieldExerciseAnswer.objects.all()
     serializer_class = UserTextfieldExerciseAnswerSerializer
 
+  
+
+class AnswerListByUser(generics.ListAPIView):
+  serializer_class = DynamicUserAnswerSerializer
+
+  def get_queryset(self):
+      user = self.kwargs["user"]
+      return UserAnswer.objects.filter(user=user)
+  
+
+class AnswerListByExercise(generics.ListAPIView):
+  serializer_class = DynamicUserAnswerSerializer
+
+  def get_queryset(self):
+      exercise = self.kwargs["exercise"]
+      return UserAnswer.objects.filter(exercise=exercise)
+  
+class AnswerListByCourse(generics.ListAPIView):
+  serializer_class = DynamicUserAnswerSerializer
+
+  def get_queryset(self):
+      course = self.kwargs["course"]
+      return UserAnswer.objects.filter(instance=course)
+  
+class AnswerListByUserAndExercise(generics.ListAPIView):
+   serializer_class = DynamicUserAnswerSerializer
+
+   def get_queryset(self):
+      user = self.kwargs["user"]
+      exercise = self.kwargs["exercise"]
+      return UserAnswer.objects.filter(user=user, exercise=exercise)
+   
+class AnswerListByUserAndCourse(generics.ListAPIView):
+   serializer_class = DynamicUserAnswerSerializer
+
+   def get_queryset(self):
+      user = self.kwargs["user"]
+      course = self.kwargs["course"]
+      return UserAnswer.objects.filter(user=user, instance=course)
+   
+class SpecificAnswer(generics.RetrieveAPIView):
+   queryset = UserAnswer.objects.all()
+   serializer_class = DynamicUserAnswerSerializer
+   lookup_field = 'id'
+
+  
+  
+
 # This is an example of custom View
-class SpesificUserAnswerListView(APIView):
+# class AnswerListViewByUser(APIView):
   
-  def get(self, request, user, format=None):
+#   def get(self, request, user, format=None):
 
-    # Filter UserAnswer objects where 'user' field matches the given username
-    user_answers = UserAnswer.objects.filter(user=user)  # Exact match
+#     # Filter UserAnswer objects where 'user' field matches the given username
+#     user_answers = UserAnswer.objects.filter(user=user)  # Exact match
 
-    if not user_answers.exists():
-      return Response({"error": "No answers found for this user."}, status=status.HTTP_404_NOT_FOUND)
+#     if not user_answers.exists():
+#       return Response({"error": "No answers found for this user."}, status=status.HTTP_404_NOT_FOUND)
 
-    serializer = DynamicUserAnswerSerializer(user_answers, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-  
-
-class SpesificExerciseAnswerListView(APIView):
-  
-  def get(self, request, exercise, format=None):
-
-    # Filter UserAnswer objects where 'user' field matches the given username
-    user_answers = UserAnswer.objects.filter(exercise=exercise)  # Exact match
-
-    if not user_answers.exists():
-      return Response({"error": "No answers found for this exercise."}, status=status.HTTP_404_NOT_FOUND)
-
-    serializer = DynamicUserAnswerSerializer(user_answers, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+#     serializer = DynamicUserAnswerSerializer(user_answers, many=True)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
