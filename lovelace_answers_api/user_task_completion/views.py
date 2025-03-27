1c701ff572beb2d2b698c3f8a2e6c501e871919e
Kasperi <kasperi.ervasti@gmail.com>
2025-03-27 15:43:50 +0200

    Add comment

--- lovelace_answers_api/user_task_completion/views.py
+++ lovelace_answers_api/user_task_completion/views.py
@@ -9,36 +9,46 @@ class UserTaskCompletionListCreate(generics.ListCreateAPIView):
     queryset = UserTaskCompletion.objects.all()
     serializer_class = UserTaskCompletionSerializer
 
+
 class UserTaskCompletionListByUser(generics.ListAPIView):
     serializer_class = UserTaskCompletionSerializer
 
     def get_queryset(self):
-        user = self.kwargs['user']
+        user = self.kwargs["user"]
         return UserTaskCompletion.objects.filter(user=user)
-    
+
+
 class UserTaskCompletionListByExercise(generics.ListAPIView):
     serializer_class = UserTaskCompletionSerializer
 
     def get_queryset(self):
-        exercise = self.kwargs['exercise']
+        exercise = self.kwargs["exercise"]
         return UserTaskCompletion.objects.filter(exercise=exercise)
-    
+
+
 class UserTaskCompletionListByCourse(generics.ListAPIView):
     serializer_class = UserTaskCompletionSerializer
 
     def get_queryset(self):
-        course = self.kwargs['course']
+        course = self.kwargs["course"]
         return UserTaskCompletion.objects.filter(instance=course)
+
 
 class UserTaskCompletionListByUserCourse(generics.ListAPIView):
     serializer_class = UserTaskCompletionSerializer
 
     def get_queryset(self):
-        user = self.kwargs['user']
-        course = self.kwargs['course']
+        user = self.kwargs["user"]
+        course = self.kwargs["course"]
         return UserTaskCompletion.objects.filter(user=user, instance=course)
 
-class UserTaskCompletionRetrieveUpdateDestroy(MultipleFieldLookupMixin, generics.RetrieveUpdateDestroyAPIView):
+
+class UserTaskCompletionRetrieveUpdateDestroy(
+    MultipleFieldLookupMixin, generics.RetrieveUpdateDestroyAPIView
+):
     queryset = UserTaskCompletion.objects.all()
     serializer_class = UserTaskCompletionSerializer
-    lookup_fields = ['user', 'exercise', 'instance']
+
+    # 'instance' used instead of 'course' because it is the field
+    # name in the model
+    lookup_fields = ["user", "exercise", "instance"]
