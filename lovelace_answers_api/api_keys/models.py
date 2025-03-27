449933974285cb2cc6f5105ca58f53fab5802cb9
Viljo Holma <viljo.holma@gmail.com>
2025-03-27 13:51:11 +0200

    edited tab sizes

--- lovelace_answers_api/api_keys/models.py
+++ lovelace_answers_api/api_keys/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 class APIKey(models.Model):
 
-  LEVEL_CHOICES = [
-    ('read', 'Only Read'),
-    ('write', 'Read + Write'),
-    ('admin', 'All access')
-  ]
+    LEVEL_CHOICES = [
+        ('read', 'Only Read'),
+        ('write', 'Read + Write'),
+        ('admin', 'All access')
+        ]
 
-  key = models.CharField(max_length=64, unique=True, blank=True)
-  level = models.CharField(max_length=5, choices=LEVEL_CHOICES)
-  created_at = models.DateTimeField(auto_now_add=True)
+    key = models.CharField(max_length=64, unique=True, blank=True)
+    level = models.CharField(max_length=5, choices=LEVEL_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-  def __str__(self):
-    return f"{self.level.upper()} API Key ({self.key[:8]}...)"
+    def __str__(self):
+        return f"{self.level.upper()} API Key ({self.key[:8]}...)"
--- lovelace_answers_api/api_keys/permissions.py
+++ lovelace_answers_api/api_keys/permissions.py
@@ -5,26 +5,27 @@ from api_keys.models import APIKey
 class HasAPIKeyPermission(BasePermission):
 
 
-  def has_permission(self, request, view):
-    
-    auth_header = request.headers.get("Authorization")
-    if not auth_header or not auth_header.startswith("Bearer "):
-      return False
-    
-    raw_key = auth_header.split(" ")[1]
 
-    hashed_key = hashlib.sha256(raw_key.encode('utf-8')).hexdigest()
+    def has_permission(self, request, view):
+    
+        auth_header = request.headers.get("Authorization")
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return False
+    
+        raw_key = auth_header.split(" ")[1]
 
-    try:
-      api_key_obj = APIKey.objects.get(key=hashed_key)
-    except APIKey.DoesNotExist:
-      return False
+        hashed_key = hashlib.sha256(raw_key.encode('utf-8')).hexdigest()
+
+        try:
+            api_key_obj = APIKey.objects.get(key=hashed_key)
+        except APIKey.DoesNotExist:
+            return False
     
-    if request.method == "GET" and api_key_obj.level in ["read", "write", "admin"]:
-        return True
-    elif request.method == "POST" and api_key_obj.level in ["write", "admin"]:
-        return True
-    elif request.method in ["PUT", "DELETE"] and api_key_obj.level == "admin":
-        return True
+        if request.method == "GET" and api_key_obj.level in ["read", "write", "admin"]:
+            return True
+        elif request.method == "POST" and api_key_obj.level in ["write", "admin"]:
+            return True
+        elif request.method in ["PUT", "DELETE"] and api_key_obj.level == "admin":
+            return True
     
-    return False
+        return False
