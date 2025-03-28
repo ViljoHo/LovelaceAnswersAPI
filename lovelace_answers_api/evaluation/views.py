6b27bd1937f9229c3e29cdc73c5ec26a2f65ef39
Kasperi <kasperi.ervasti@gmail.com>
2025-03-28 18:38:49 +0200

    Add url to location header + format

--- lovelace_answers_api/evaluation/views.py
+++ lovelace_answers_api/evaluation/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from rest_framework import generics, status
 from rest_framework.response import Response
 
@@ -7,29 +8,35 @@ from api_keys.permissions import HasAPIKeyPermission
 from user_answer.models import UserAnswer
 from .serializers import EvaluationSerializer
 
+
 class EvaluationList(generics.ListAPIView):
     queryset = Evaluation.objects.all()
     serializer_class = EvaluationSerializer
     permission_classes = [HasAPIKeyPermission]
+
 
 class EvaluationListByStaff(generics.ListAPIView):
     serializer_class = EvaluationSerializer
     permission_classes = [HasAPIKeyPermission]
 
     def get_queryset(self):
-        user = self.kwargs['user']
+        user = self.kwargs["user"]
         return Evaluation.objects.filter(evaluator=user)
+
 
 class EvaluationCreate(generics.CreateAPIView):
     serializer_class = EvaluationSerializer
     permission_classes = [HasAPIKeyPermission]
 
     def create(self, request, *args, **kwargs):
-        answer_id = self.kwargs['answer']
+        answer_id = self.kwargs["answer"]
         user_answer = get_object_or_404(UserAnswer, pk=answer_id)
 
         if user_answer.evaluation:
-            return Response({"error": "Evaluation already exists for this answer."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Evaluation already exists for this answer."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         serializer = self.get_serializer(data=request.data)
 
@@ -37,12 +44,23 @@ class EvaluationCreate(generics.CreateAPIView):
             evaluation = serializer.save()
             user_answer.evaluation = evaluation
             user_answer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
+            location_url = reverse(
+                "get-put-patch-delete-evaluation",
+                kwargs={"id": evaluation.id},
+            )
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+                headers={"Location": location_url},
+            )
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
+
+
 class EvaluationRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Evaluation.objects.all()
     serializer_class = EvaluationSerializer
-    lookup_field = 'id'
+    lookup_field = "id"
     permission_classes = [HasAPIKeyPermission]
