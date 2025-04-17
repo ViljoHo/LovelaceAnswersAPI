56c321b0776b60e4dffd57e814fb1a2313f98ade
Kasperi <kasperi.ervasti@gmail.com>
2025-04-17 19:55:01 +0300

    Add forgotten /

--- lovelace_answers_api/for_testing/urls.py
+++ lovelace_answers_api/for_testing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("reset/answers", views.ResetUserAnswersView.as_view(), name="reset-answers"),
-    path("reset/evaluations", views.ResetEvaluationsView.as_view(), name="reset-evaluations"),
-    path("reset/completions", views.ResetUserTaskCompletionsView.as_view(), name="reset-completions")
+    path("reset/answers/", views.ResetUserAnswersView.as_view(), name="reset-answers"),
+    path("reset/evaluations/", views.ResetEvaluationsView.as_view(), name="reset-evaluations"),
+    path("reset/completions/", views.ResetUserTaskCompletionsView.as_view(), name="reset-completions")
 ]
